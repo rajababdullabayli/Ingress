6affa0b1ea6ca6f42407463cd8095fcbae019577
Receb08 <receb.abdullabayli@gmail.com>
2026-07-16 20:16:36 +0400

    Add Main class for book input and display

---
+++ Java/OOP _Recursion_Tasks/Task_2/Main.java
@@ -0,0 +1,28 @@
+package Task2;
+
+import java.util.Scanner;
+
+public class Main {
+    static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the Book's number");
+        int number = input.nextInt();
+        String [] arrayTitle = new String[number];
+        String [] arrayAuthor = new String[number];
+        for (int i=0;i<number;i++){
+            System.out.println("Enter the Book's Title");
+            String title = input.next();
+            arrayTitle[i] = title;
+            System.out.println("Enter the Book's Author");
+            String author = input.next();
+            arrayAuthor[i] = author;
+        }
+        for (int j=0;j<number;j++){
+            Book book = new Book();
+            book.setAuthor(arrayAuthor[j]);
+            book.setTitle(arrayTitle[j]);
+            book.getInfo();
+        }
+        System.gc();
+    }
+}
