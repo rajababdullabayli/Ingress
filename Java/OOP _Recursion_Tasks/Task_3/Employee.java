aeb657a943bfb9e6703998fb2212164875f8b870
Receb08 <receb.abdullabayli@gmail.com>
2026-07-16 20:17:18 +0400

    Create Employee.java

---
+++ Java/OOP _Recursion_Tasks/Task_3/Employee.java
@@ -0,0 +1,30 @@
+package Task3;
+
+public class Employee {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void getInfo() {
+        System.out.println("Id: " + id);
+        System.out.println("Name: " + name);
+    }
+
+    public Employee() {
+    }
+}
