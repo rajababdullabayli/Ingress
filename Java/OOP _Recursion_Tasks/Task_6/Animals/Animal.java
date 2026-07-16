5d90b4e3a1431134738b682c770bfa67bc45c2f6
Receb08 <receb.abdullabayli@gmail.com>
2026-07-16 20:50:41 +0400

    Add Animal class with various method access levels

---
+++ Java/OOP _Recursion_Tasks/Task_6/Animals/Animal.java
@@ -0,0 +1,19 @@
+package Task6.Animals;
+
+public class Animal {
+    public void publicMethod() {
+        System.out.println("Public method is working");
+    }
+
+    protected void protectedMethod() {
+        System.out.println("Protected method is not working");
+    }
+
+    private void privateMethod() {
+        System.out.println("Private method is not working");
+    }
+
+    void defaultMethod() {
+        System.out.println("Default  method is not working");
+    }
+}
