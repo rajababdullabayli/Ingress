7a55b8f25cae1db6c2fba865cb835c6da6bdf6b5
Receb08 <receb.abdullabayli@gmail.com>
2026-07-16 20:17:49 +0400

    Add Main class for employee input and display
    Implemented a main class to input employee details and display information.

---
+++ Java/OOP _Recursion_Tasks/Task_3/Main.java
@@ -0,0 +1,29 @@
+package Task3;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter  the Employee's number");
+        int number = input.nextInt();
+        String employeename;
+        int employeeId;
+        String[] array = new String[number];
+        int[] arrayId = new int[number];
+        for (int i = 0; i < number; i++) {
+            System.out.println("Enter the Employee's Name");
+            employeename = input.next();
+            array[i] = employeename;
+            arrayId[i] = i + 1;
+        }
+        for (int j = 0; j < number; j++) {
+            Employee employee = new Employee();
+            employee.setName(array[j]);
+            employee.setId(arrayId[j]);
+            employee.getInfo();
+        }
+
+    }
+}
