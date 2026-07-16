d3dbbaa82248aa12bec727b8694f021d2371ef44
Receb08 <receb.abdullabayli@gmail.com>
2026-07-16 20:48:42 +0400

    Create BankAccount.java

---
+++ Java/OOP _Recursion_Tasks/Task_5/BankAccount.java
@@ -0,0 +1,50 @@
+package Task5;
+
+public class BankAccount {
+    public String bankName;
+    protected String owner;
+    private double balance;
+    int accountNumber;
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public void getInfo(){
+        System.out.println("BankName: "+bankName);
+        System.out.println("Owner: "+owner);
+        System.out.println("Balance: "+balance);
+        System.out.println("AccountNumber: "+accountNumber);
+    }
+
+    public BankAccount() {
+    }
+}
