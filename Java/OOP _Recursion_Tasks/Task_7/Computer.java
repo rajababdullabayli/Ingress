2a99256d3c155dbfaba59b382a9a02640b5c6b4d
Receb08 <receb.abdullabayli@gmail.com>
2026-07-16 20:53:14 +0400

    Create Computer.java

---
+++ Java/OOP _Recursion_Tasks/Task_7/Computer.java
@@ -0,0 +1,35 @@
+package Task7;
+
+public class Computer {
+    public static class Processor {
+        private String model;
+        private int cores;
+
+        public Processor() {
+        }
+
+        public String getModel() {
+            return model;
+        }
+
+        public int getCores() {
+            return cores;
+        }
+
+        public void setModel(String model) {
+            this.model = model;
+        }
+
+        public void setCores(int cores) {
+            this.cores = cores;
+        }
+
+        public void printInfo() {
+            System.out.println("Processor Model: " + model);
+            System.out.println("Processor Cores:" + cores);
+        }
+    }
+
+    public Computer() {
+    }
+}
