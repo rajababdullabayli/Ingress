7bbc2b65dc1e2be85acae52d0c892a9944530442
Receb08 <receb.abdullabayli@gmail.com>
2026-07-16 21:16:57 +0400

    Create Main.java

---
+++ Java/OOP _Recursion_Tasks/Task_12/Main.java
@@ -0,0 +1,21 @@
+package Task12;
+
+import java.util.Scanner;
+
+public class Main {
+    static int fibonacci(int numb) {
+        if (numb <= 1) {
+            return numb;
+        }
+        return fibonacci(numb - 1) + fibonacci(numb - 2);
+    }
+
+    static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number to find it ");
+        int numb = input.nextInt();
+        for (int i = 0; i < numb; i++) {
+            System.out.println(fibonacci(i) + " ");
+        }
+    }
+}
