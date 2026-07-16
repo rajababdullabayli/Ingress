c1612cc86b0f75633f3e2c3c0725a8d97aef928e
Receb08 <receb.abdullabayli@gmail.com>
2026-07-16 20:15:18 +0400

    Fix formatting and remove unnecessary newlines

---
+++ Java/OOP _Recursion_Tasks/Task_1/Student.java
@@ -0,0 +1,31 @@
+package Task1;
+
+public class Student {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+    public void printInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+
+    public Student() {
+    }
+}
