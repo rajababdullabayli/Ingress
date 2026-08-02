6d3e75d2e69f5397bf2f2bd0f3d6c97a089847fe
Rajab Abdullabayli <receb.abdullabayli@gmail.com>
2026-08-02 15:41:35 +0400

    Create MemoryDemo.java

---
+++ Java/OOP _Recursion_Tasks/Task_1/MemoryDemo.java
@@ -0,0 +1,31 @@
+package Task_1;
+
+import java.util.Scanner;
+
+public class MemoryDemo {
+    int number;
+    String name;
+    String student;
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter The Student's number");
+        int number = input.nextInt();
+        String[] arrayName = new String[number];
+        int[] arrayAge = new int[number];
+        for (int i = 0; i < number; i++) {
+            System.out.println("Enter The Student's name:");
+            String name = input.next();
+            System.out.println("Enter The Student's  age:");
+            int age = input.nextInt();
+            arrayName[i] = name;
+            arrayAge[i] = age;
+        }
+        for (int j = 0; j < number; j++) {
+            Student student = new Student();
+            student.setName(arrayName[j]);
+            student.setAge(arrayAge[j]);
+            student.printInfo();
+        }
+    }
+}
